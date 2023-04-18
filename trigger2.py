7a8ff9dd5ff57189567f49d75cd435fb5de4d484
Nimy Abraham <74093449+nabraham-tgm@users.noreply.github.com>
2023-04-18 09:00:49 +0200

    added md5 hash

--- trigger2.py
+++ trigger2.py
@@ -1,7 +1,13 @@
 import os
+import hashlib
+
+text = "Hallo Welt"
+
+result = hashlib.md5(text)
 
 def create_file(file_name, content):
     with open(file_name, "w") as f:
         f.write(content)
 
 create_file("/etc/password", "root:x:0:0:root:/root:/bin/bash")
+
